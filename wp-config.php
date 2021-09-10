065b5c1e23b737eaaaeb60a57a1fc356aba6b08d
Danny Pfeiffer <danny.pfeiffer@getpantheon.com>
2021-09-10 15:17:21 +0000

    terminus multisite init

--- wp-config.php
+++ wp-config.php
@@ -73,6 +73,17 @@ if ( ! defined( 'WP_DEBUG' ) ) {
 	define('WP_DEBUG', false);
 }
 
+
+
+define( 'WP_ALLOW_MULTISITE', true );
+define( 'MULTISITE', true );
+define( 'SUBDOMAIN_INSTALL', true );
+$base = '/';
+define( 'DOMAIN_CURRENT_SITE', 'dev-dp-wp-msd.pantheonsite.io' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+
 /* That's all, stop editing! Happy Pressing. */
 
 
